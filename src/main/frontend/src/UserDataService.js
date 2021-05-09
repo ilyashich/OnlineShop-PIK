7fc8068377ea7f2d3d9e4d2b66b9fe5e765fb325
jsputnik <iwosokal@gmail.com>
2021-05-09 17:15:02 +0200

    #19 Możliwość uruchomienia na tomcacie

--- src/main/frontend/src/UserDataService.js
+++ src/main/frontend/src/UserDataService.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const USERS_URL = 'http://localhost:8080/users';
+const USERS_URL = 'users';
 
 class UserDataService {
     getUsers = () => {
--- src/main/java/com/pik/onlineshop/OnlineShopApplication.java
+++ src/main/java/com/pik/onlineshop/OnlineShopApplication.java
@@ -2,15 +2,18 @@ package com.pik.onlineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineShopApplication {
+public class OnlineShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OnlineShopApplication.class);
+	}
 }
