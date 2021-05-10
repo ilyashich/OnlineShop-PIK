5c7145694fb66374a9391877e1e70663ac24a78c
Szymon Piotr Kisiel <s.kisiel@stud.elka.pw.edu.pl>
2021-05-10 15:37:17 +0200

    Connection to example movie graph.

---
+++ src/main/java/com/pik/onlineshop/movies/Movie.java
@@ -0,0 +1,40 @@
+package com.pik.onlineshop.movies;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+/**
+ * @author Mark Angrish
+ * @author Michael J. Simons
+ */
+@Node
+public class Movie {
+
+	@Id
+	private final String title;
+
+	private final String tagline;
+
+	private Integer released;
+
+	public Movie(String title, String tagline) {
+		this.title = title;
+		this.tagline = tagline;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getTagline() {
+		return tagline;
+	}
+
+	public Integer getReleased() {
+		return released;
+	}
+
+	public void setReleased(Integer released) {
+		this.released = released;
+	}
+}
