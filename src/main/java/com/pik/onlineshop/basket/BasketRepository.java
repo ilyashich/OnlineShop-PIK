8ecc1fae33ab7b0153cc51a3cfa2eddab7f4c922
Szymon Piotr Kisiel <s.kisiel@stud.elka.pw.edu.pl>
2021-05-30 16:36:56 +0200

    #63 #86 #89 Fix BasketRepository methods

--- src/main/java/com/pik/onlineshop/basket/BasketRepository.java
+++ src/main/java/com/pik/onlineshop/basket/BasketRepository.java
@@ -10,8 +10,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 interface BasketRepository extends Neo4jRepository<Basket, Integer> {
-    Basket showBasket(@Param("name") String customerName);
-    Basket addProduct(@Param("name") String productName);
-    Basket deleteProduct(@Param("name") String productName);
+    Basket showBasket(@Param("customerName") String customerName);
+    Basket addProduct(@Param("customerName") String customerName, @Param("productName") String productName);
+    Basket deleteProduct(@Param("customerName") String customerName, @Param("productName") String productName);
 }
 
