f8222c5282c65b847e13111ee84e3e10b3111254
Szymon Piotr Kisiel <s.kisiel@stud.elka.pw.edu.pl>
2021-05-30 16:58:52 +0200

    #63 #86 #89 BasketRepository queries

--- src/main/java/com/pik/onlineshop/basket/BasketRepository.java
+++ src/main/java/com/pik/onlineshop/basket/BasketRepository.java
@@ -10,8 +10,27 @@ import java.time.LocalDate;
 import java.util.List;
 
 interface BasketRepository extends Neo4jRepository<Basket, Integer> {
+    @Query("MATCH (customer:Customer)-[:CURRENT]->(basket:Basket) WHERE customer.login = $customerLogin RETURN basket")
+    // TODO: Create basket if doesn't exist
     Basket showBasket(@Param("customerLogin") String customerLogin);
+
+    @Query("MATCH (customer:Customer)-[:CURRENT]->(basket:Basket)\n" +
+            "WHERE customer.login = $customerLogin\n" +
+            "MATCH (product:Product)\n" +
+            "WHERE product.name = $productName\n" +
+            "WITH basket, product\n" +
+            "CREATE (basket)<-[:IN {amount: 1}]-(product) \n" +
+            "RETURN basket")
     Basket addProduct(@Param("customerLogin") String customerLogin, @Param("productName") String productName);
+
+    @Query("MATCH (customer:Customer)-[:CURRENT]->(basket:Basket)\n" +
+            "WHERE customer.login = $customerLogin\n" +
+            "MATCH (product:Product)\n" +
+            "WHERE product.name = $productName\n" +
+            "WITH basket, product\n" +
+            "MATCH (basket)<-[r:IN]-(product) \n" +
+            "DELETE r\n" +
+            "RETURN basket")
     Basket deleteProduct(@Param("customerLogin") String customerLogin, @Param("productName") String productName);
 }
 
